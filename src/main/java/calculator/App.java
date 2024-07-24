44cab736c465a1d829f049aec92dc1dce209976a
KDG <rlaehrbs9702@naver.com>
2024-07-24 12:44:41 +0900

    LV 1 사칙연산 결과 도출 구현(요구사항 3)

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -21,6 +21,31 @@ public class App {
         char operator = sc.next().charAt(0);
 //        System.out.println("operator = " + operator);// 사칙 연산 기호 입력
 
+        int result = 0;
+
+        /* swtich 제어문으로 입력받은 식의 사칙연산을 진행하여 결과값 도출*/
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("결과: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("결과: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("결과: " + result);
+                break;
+            case '/':
+                if (num2 == 0) System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                result = num1 / num2;
+                System.out.println("결과:" + result);
+                break;
+            default:
+                System.out.println("올바른 사칙 연산 기호(+, -, *, /)를 입력해 주세요!");
+        }
+
 
     }
 }
