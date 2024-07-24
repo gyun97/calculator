2a950597f90b8717d214305457934433990c5273
KDG <rlaehrbs9702@naver.com>
2024-07-24 12:10:18 +0900

    Lv1 숫자 및 사칙 연산 기호 입력 구현(요구 사항 1 ~ 2)

---
+++ src/main/java/calculator/App.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt(); // 첫 번째 숫자 입력
+        System.out.println("첫 번째 숫자를 입력하세요:");
+
+        int num2 = sc.nextInt(); // 두 번째 숫자 입력
+        System.out.println("두 번째 숫자를 입력하세요:");
+
+        char operator = sc.next().charAt(0); // 사칙 연산 기호 입력
+        System.out.println("사칙 연산 기호를 입력하세요:");
+
+
+    }
+}
